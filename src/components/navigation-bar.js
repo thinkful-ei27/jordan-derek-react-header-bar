3593f04965b3e094829a0cdebf5873e862afdbb7
Glitch (functional-porpoise) <none>
2019-01-22 08:34:14 -0800

    Create NavigationBar component

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,6 +1,20 @@
+'use strict';
+
 import React from 'react';
 
 import './navigation-bar.css';
 
 // The NavigationBar component goes here.  It should be the default export.
-
+export default function NavigationBar(props) {
+  const links = props.links.map(link => <li><a href={link.href}>{link.text}</a></li>);
+  return (
+    <section className="navigation-bar">
+      <h1>{props.title}</h1>
+      <nav className="navigation-bar-nav">
+        <ul>
+          {links}
+        </ul>
+      </nav>
+    </section>
+  );
+}
